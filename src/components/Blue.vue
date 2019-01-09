de7b1c61f8f8938c1bc36e9f2eee56dec9fcc944
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2019-01-09 20:00:32 +0000

    first commit

---
+++ src/components/Blue.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="blue">
+        <h5 class=""> {{message}} </h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blue',
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style>
+h5 {
+    background-color: blue;
+}
+</style>
